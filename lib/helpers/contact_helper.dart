a346c4c0d18484f986c6ca27488218e968109881
Thais <thaiscarvalho115@gmail.com>
2021-06-05 13:59:25 -0300

    get contact

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -46,6 +46,15 @@ class ContactHelper
     contact.id = await dbContact.insert(contactTable, contact.toMap());
     return contact;
   }
+  Future<Contact> getContact(int id) async
+  {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable, columns: [idColumn, nameColumn, emailColumn, phoneColumn, imageColumn], where: "$idColumn = ?", whereArgs: [id]);
+    if(maps.length > 0)
+      return Contact.fromMap(maps.first);
+    else
+      return null;
+  }
 }
 
 class Contact
